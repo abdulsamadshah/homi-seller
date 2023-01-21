fd72736c9a8a1d09f5fa07103d340b53e9b83d79
Chandu <chandugangola999@gmail.com>
2023-01-21 12:18:10 +0530

    datepicker updated

--- lib/app/shared/widgets/multipledatepicker.dart
+++ lib/app/shared/widgets/multipledatepicker.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:homlisellerapp/app/View/datepicker.dart';
+import 'package:homlisellerapp/app/shared/color.dart';
 import 'package:intl/intl.dart';
-import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class MultipleDatePicker extends StatefulWidget {
   const MultipleDatePicker({super.key});
@@ -11,23 +10,62 @@ class MultipleDatePicker extends StatefulWidget {
 }
 
 class _MultipleDatePickerState extends State<MultipleDatePicker> {
+  TextEditingController dateinput = TextEditingController();
+
+  @override
+  void initState() {
+    dateinput.text;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: () {
-        SfDateRangePicker(
-          view: DateRangePickerView.month,
-          monthViewSettings:
-              DateRangePickerMonthViewSettings(firstDayOfWeek: 6),
-          selectionMode: DateRangePickerSelectionMode.multiple,
-          enableMultiView: true,
-          showActionButtons: true,
-          onSubmit: (val) {
-            print(val);
-          },
-        );
-      },
-      child: Text(DateTime.now.toString()),
+    return Container(
+      height: 25,
+      width: 120,
+      child: TextField(
+        controller: dateinput,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          prefixIcon: Icon(
+            Icons.calendar_today,
+            size: 12,
+          ),
+        ),
+        style: TextStyle(fontSize: 8, color: Colors.black),
+        readOnly: true,
+        onTap: () async {
+          DateTime? pickeddate = await showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2000),
+            lastDate: DateTime(2101),
+            builder: (context, child) => Theme(
+              data: ThemeData().copyWith(
+                colorScheme: ColorScheme.dark(
+                  primary: FixedColors.purple,
+                  surface: Colors.white,
+                  onSurface: Colors.black,
+                ),
+              ),
+              child: child!,
+            ),
+            confirmText: 'Apply',
+            cancelText: 'cancel',
+          );
+
+          if (pickeddate != null) {
+            String formattedDate = DateFormat("dd/mm/yyyy").format(pickeddate);
+            setState(
+              () {
+                dateinput.text = formattedDate.toString();
+              },
+            );
+          } else {
+            print('Not seleccted');
+          }
+        },
+      ),
     );
   }
 }
